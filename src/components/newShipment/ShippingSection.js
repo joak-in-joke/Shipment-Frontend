051c4fe3ad79fc13309fdcb47a8f6984010a4145
benjaminignc <benjamin.contrerass@mail.udp.cl>
2021-06-19 23:08:06 -0400

    Form size fixes + RIP Aweonao

--- src/components/newShipment/ShippingSection.js
+++ src/components/newShipment/ShippingSection.js
@@ -37,7 +37,7 @@ const ShippingSection = () => {
     <Grid className={classes.rootSection}>
       Datos de envío
       <Grid container spacing={2}>
-        <Grid item xs={2}>
+        <Grid item xs>
           <Controller
             control={control}
             name="exportador"
@@ -66,7 +66,7 @@ const ShippingSection = () => {
           />
         </Grid>
 
-        <Grid item xs={2}>
+        <Grid item xs>
           <Controller
             control={control}
             name="importador"
@@ -95,7 +95,7 @@ const ShippingSection = () => {
           />
         </Grid>
 
-        <Grid item xs={2}>
+        <Grid item xs>
           <Controller
             control={control}
             name="operador"
@@ -111,7 +111,7 @@ const ShippingSection = () => {
           />
         </Grid>
 
-        <Grid item xs={2}>
+        <Grid item xs>
           <Controller
             control={control}
             name="agencia"
@@ -126,17 +126,66 @@ const ShippingSection = () => {
             )}
           />
         </Grid>
+      </Grid>
+      {(transportWatch === "LCL" || transportWatch === "FCL") && (
+        <Grid container spacing={2}>
+          {tipeWatch === "exportacion" && (
+            <Grid item xs>
+              <Controller
+                control={control}
+                name="reserva"
+                render={({ field: { onChange, value } }) => (
+                  <TextField
+                    label="Número de reserva"
+                    variant="outlined"
+                    value={value}
+                    className={classes.formControl}
+                    onChange={onChange}
+                  />
+                )}
+              />
+            </Grid>
+          )}
 
-        {(transportWatch === "LCL" || transportWatch === "FCL") && (
-          <>
-            {tipeWatch === "exportacion" && (
-              <Grid item xs={2}>
+          {transportWatch === "LCL" && (
+            <>
+              <Grid item xs>
                 <Controller
                   control={control}
-                  name="reserva"
+                  name="tipoDocumento"
+                  render={({ field: { onChange, value } }) => (
+                    <FormControl
+                      variant="outlined"
+                      className={classes.formControl}
+                    >
+                      <InputLabel id="tipoDocumentolabel">
+                        Tipo de documento
+                      </InputLabel>
+                      <Select
+                        labelId="tipoDocumentolabel"
+                        value={value}
+                        onChange={onChange}
+                        label="Tipo de documento"
+                      >
+                        <MenuItem value="">
+                          <em>Seleccionar</em>
+                        </MenuItem>
+                        <MenuItem value="MBL">MBL</MenuItem>
+                        <MenuItem value="HBL">HBL</MenuItem>
+                        <MenuItem value="NBL">NBL</MenuItem>
+                      </Select>
+                    </FormControl>
+                  )}
+                />
+              </Grid>
+
+              <Grid item xs={8}>
+                <Controller
+                  control={control}
+                  name="documento"
                   render={({ field: { onChange, value } }) => (
                     <TextField
-                      label="Número de reserva"
+                      label="Número de Documento"
                       variant="outlined"
                       value={value}
                       className={classes.formControl}
@@ -145,132 +194,83 @@ const ShippingSection = () => {
                   )}
                 />
               </Grid>
-            )}
+            </>
+          )}
 
-            {transportWatch === "LCL" && (
-              <>
-                <Grid item xs={2}>
-                  <Controller
-                    control={control}
-                    name="tipoDocumento"
-                    render={({ field: { onChange, value } }) => (
-                      <FormControl
-                        variant="outlined"
-                        className={classes.formControl}
+          {transportWatch === "FCL" && (
+            <>
+              <Grid item xs>
+                <Controller
+                  control={control}
+                  name="depositoContenedores"
+                  render={({ field: { onChange, value } }) => (
+                    <TextField
+                      label="Depósito de Contenedores"
+                      variant="outlined"
+                      value={value}
+                      className={classes.formControl}
+                      onChange={onChange}
+                    />
+                  )}
+                />
+              </Grid>
+
+              <Grid item xs={2}>
+                <Controller
+                  control={control}
+                  name="contTipo"
+                  render={({ field: { onChange, value } }) => (
+                    <FormControl
+                      variant="outlined"
+                      className={classes.formControl}
+                    >
+                      <InputLabel id="contTipolabel">Tipo</InputLabel>
+                      <Select
+                        labelId="contTipolabel"
+                        value={value}
+                        onChange={onChange}
+                        label="Tipo"
                       >
-                        <InputLabel id="tipoDocumentolabel">
-                          Tipo de documento
-                        </InputLabel>
-                        <Select
-                          labelId="tipoDocumentolabel"
-                          value={value}
-                          onChange={onChange}
-                          label="Tipo de documento"
-                        >
-                          <MenuItem value="">
-                            <em>Seleccionar</em>
-                          </MenuItem>
-                          <MenuItem value="MBL">MBL</MenuItem>
-                          <MenuItem value="HBL">HBL</MenuItem>
-                          <MenuItem value="NBL">NBL</MenuItem>
-                        </Select>
-                      </FormControl>
-                    )}
-                  />
-                </Grid>
+                        <MenuItem value="">
+                          <em>Seleccionar</em>
+                        </MenuItem>
+                        <MenuItem value="0">Sin container</MenuItem>
+                        <MenuItem value="1">20’ Flat Rack</MenuItem>
+                        <MenuItem value="2">20’ Open Top</MenuItem>
+                        <MenuItem value="3">20’ Standard</MenuItem>
+                        <MenuItem value="4">40’ Flat Rack</MenuItem>
+                        <MenuItem value="5">40’ High Cube</MenuItem>
+                        <MenuItem value="6">40’ NOR</MenuItem>
+                        <MenuItem value="7">40’ Open Top</MenuItem>
+                        <MenuItem value="8">40’ Reefer</MenuItem>
+                        <MenuItem value="9">40’ Standard</MenuItem>
+                      </Select>
+                    </FormControl>
+                  )}
+                />
+              </Grid>
 
-                <Grid item xs={6}>
-                  <Controller
-                    control={control}
-                    name="documento"
-                    render={({ field: { onChange, value } }) => (
-                      <TextField
-                        label="Número de Documento"
-                        variant="outlined"
-                        value={value}
-                        className={classes.formControl}
-                        onChange={onChange}
-                      />
-                    )}
-                  />
-                </Grid>
-              </>
-            )}
-
-            {transportWatch === "FCL" && (
-              <>
-                <Grid item xs={2}>
-                  <Controller
-                    control={control}
-                    name="depositoContenedores"
-                    render={({ field: { onChange, value } }) => (
-                      <TextField
-                        label="Depósito de Contenedores"
-                        variant="outlined"
-                        value={value}
-                        className={classes.formControl}
-                        onChange={onChange}
-                      />
-                    )}
-                  />
-                </Grid>
-
-                <Grid item xs={2}>
-                  <Controller
-                    control={control}
-                    name="contTipo"
-                    render={({ field: { onChange, value } }) => (
-                      <FormControl
-                        variant="outlined"
-                        className={classes.formControl}
-                      >
-                        <InputLabel id="contTipolabel">Tipo</InputLabel>
-                        <Select
-                          labelId="contTipolabel"
-                          value={value}
-                          onChange={onChange}
-                          label="Tipo"
-                        >
-                          <MenuItem value="">
-                            <em>Seleccionar</em>
-                          </MenuItem>
-                          <MenuItem value="0">Sin container</MenuItem>
-                          <MenuItem value="1">20’ Flat Rack</MenuItem>
-                          <MenuItem value="2">20’ Open Top</MenuItem>
-                          <MenuItem value="3">20’ Standard</MenuItem>
-                          <MenuItem value="4">40’ Flat Rack</MenuItem>
-                          <MenuItem value="5">40’ High Cube</MenuItem>
-                          <MenuItem value="6">40’ NOR</MenuItem>
-                          <MenuItem value="7">40’ Open Top</MenuItem>
-                          <MenuItem value="8">40’ Reefer</MenuItem>
-                          <MenuItem value="9">40’ Standard</MenuItem>
-                        </Select>
-                      </FormControl>
-                    )}
-                  />
-                </Grid>
-
-                <Grid item xs={2}>
-                  <Controller
-                    control={control}
-                    name="sello"
-                    render={({ field: { onChange, value } }) => (
-                      <TextField
-                        label="Sello"
-                        variant="outlined"
-                        value={value}
-                        className={classes.formControl}
-                        onChange={onChange}
-                      />
-                    )}
-                  />
-                </Grid>
-              </>
-            )}
-          </>
-        )}
-
-        <Grid item>
+              <Grid item xs>
+                <Controller
+                  control={control}
+                  name="sello"
+                  render={({ field: { onChange, value } }) => (
+                    <TextField
+                      label="Sello"
+                      variant="outlined"
+                      value={value}
+                      className={classes.formControl}
+                      onChange={onChange}
+                    />
+                  )}
+                />
+              </Grid>
+            </>
+          )}
+        </Grid>
+      )}
+      <Grid container spacing={2}>
+        <Grid item xs>
           <Controller
             control={control}
             name="motonave"
@@ -286,7 +286,7 @@ const ShippingSection = () => {
           />
         </Grid>
 
-        <Grid item>
+        <Grid item xs>
           <Controller
             control={control}
             name="viaje"
@@ -302,7 +302,7 @@ const ShippingSection = () => {
           />
         </Grid>
 
-        <Grid item>
+        <Grid item xs>
           <Controller
             control={control}
             name="naviera"
@@ -319,7 +319,7 @@ const ShippingSection = () => {
         </Grid>
 
         {transportWatch === "LCL" && (
-          <Grid item xs={4}>
+          <Grid item xs>
             <Controller
               control={control}
               name="almacen"
@@ -335,8 +335,9 @@ const ShippingSection = () => {
             />
           </Grid>
         )}
-
-        {transportWatch === "LCL" && (
+      </Grid>
+      {transportWatch === "LCL" && (
+        <Grid container spacing={2}>
           <>
             <Grid item xs={6}>
               <Controller
@@ -354,7 +355,7 @@ const ShippingSection = () => {
               />
             </Grid>
 
-            <Grid item xs={2}>
+            <Grid item xs>
               <Controller
                 control={control}
                 name="bultos"
@@ -371,7 +372,7 @@ const ShippingSection = () => {
               />
             </Grid>
 
-            <Grid item xs={2}>
+            <Grid item xs>
               <Controller
                 control={control}
                 name="peso"
@@ -393,7 +394,7 @@ const ShippingSection = () => {
               />
             </Grid>
 
-            <Grid item xs={2}>
+            <Grid item xs>
               <Controller
                 control={control}
                 name="volumen"
@@ -415,8 +416,8 @@ const ShippingSection = () => {
               />
             </Grid>
           </>
-        )}
-      </Grid>
+        </Grid>
+      )}
     </Grid>
   );
 };
